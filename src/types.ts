b287d7009cb717c2917f765dcec028607653d430
Joshua Luce <joshuabluce@protonmail.com>
2020-12-04 20:48:47 -0600

    Create types helper file

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface StockInfo {
+    symbol: string;
+    name: string;
+    exchange: string;
+    currentPrice: number;
+    highPrice: number;
+    lowPrice: number;
+    meanPrice: number;
+    medianPrice: number;
+}
